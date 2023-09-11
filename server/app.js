e7344eafa319fc0d2e8d9c9b19df06fcfbfcca61
Jacob Moses <jacob@alarmin.com.au>
2023-09-11 22:11:59 +1000

    Get all subjects

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import course from "./routes/course.js";
 import spk from "./routes/spk.js";
+import subject from "./routes/subject.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/course", course);
 app.use("/spk", spk);
+app.use("/subject", subject);
 
 export default app;
