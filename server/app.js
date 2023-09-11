10b479f7c37c7d6cb06b492e2e31432e368ea316
Jacob Moses <jacob@alarmin.com.au>
2023-09-11 22:09:36 +1000

    Get all spks

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import course from "./routes/course.js";
+import spk from "./routes/spk.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/course", course);
+app.use("/spk", spk);
 
 export default app;
