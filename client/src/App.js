9a26adf0f17555f99268871c3390432819e8086f
Jacob Moses <jacob@alarmin.com.au>
2023-09-18 20:24:32 +1000

    client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/navbar";
+import Search from "./components/search.js";
+import Course from "./components/course.js";
+import Spk from "./components/spk.js";
+import Subject from "./components/subject.js";
 
 const App = () => {
   return(
     <div>
       <Navbar />
-
+      <div style={{ margin: 20 }}>
+      <Routes>
+        <Route exact path="/" element={<Search />} />
+        <Route path="/course/:id" element={<Course />} />
+        <Route path="/spk/:id" element={<Spk />} />
+        <Route path="/subject/:id" element={<Subject />} />
+      </Routes>
+      </div>
     </div>
   );
 };
